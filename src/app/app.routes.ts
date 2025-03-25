865830ac02c54429f2a7e778ea438528ea635c15
Aldo Angeles <111522210+aaldoaangeles@users.noreply.github.com>
2025-03-25 15:05:10 -0600

    feat: creación de componente template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/layout/login/component/login.component';
 import { RegisterComponent } from './modules/layout/register/register.component';
+import { TemplateComponent } from './modules/layout/template/template.component';
 
 export const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  {
+    path: '',
+    component: TemplateComponent,
+    children: [
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 
---
+++ src/app/modules/layout/template/template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-template',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './template.component.html',
+  styleUrl: './template.component.css'
+})
+export class TemplateComponent {
+
+}
