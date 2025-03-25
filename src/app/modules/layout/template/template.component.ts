f6cd2929b3df3d2631f75159cecb08430907df6a
Aldo Angeles <111522210+aaldoaangeles@users.noreply.github.com>
2025-03-25 16:16:52 -0600

    feat: se agrega lógica de usuario loggeado en el header

--- src/app/modules/layout/template/template.component.ts
+++ src/app/modules/layout/template/template.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './template.component.html',
   styleUrl: './template.component.css'
 })
 export class TemplateComponent {
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
 
+  cerrarSesion() {
+    localStorage.removeItem('token'); // o tu lógica real de logout
+    window.location.href = '/'; // o usa router.navigate(['/']);
+  }
 }
