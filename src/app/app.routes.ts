9e228b373bb06bd9d8951ed2c8e3302ee6dd721a
arletpinacho <arlet.pinacho@ciencias.unam.mx>
2025-04-15 22:41:23 -0600

    refactor: renombra la carpeta de las vistas relacionadas al usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './modules/layout/login/login.component';
-import { RegisterComponent } from './modules/layout/register/register.component';
-import { TemplateComponent } from './modules/layout/template/template.component';
-import { ProfileComponent } from './modules/layout/profile/profile.component';
+import { LoginComponent } from './modules/usuario/login/login.component';
+import { RegisterComponent } from './modules/usuario/register/register.component';
+import { TemplateComponent } from './modules/usuario/template/template.component';
+import { ProfileComponent } from './modules/usuario/profile/profile.component';
 export const routes: Routes = [
   {
     path: '',
