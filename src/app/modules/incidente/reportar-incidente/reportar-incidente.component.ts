8beab27f6a84470c0b12a0053fb5c4dd164d6208
Aldo <110924892+andreauxue@users.noreply.github.com>
2025-04-27 00:15:03 -0600

    feat: crear componenete para reportar incidentes

---
+++ src/app/modules/incidente/reportar-incidente/reportar-incidente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reportar-incidente',
+  imports: [],
+  templateUrl: './reportar-incidente.component.html',
+  styleUrl: './reportar-incidente.component.css'
+})
+export class ReportarIncidenteComponent {
+
+}
