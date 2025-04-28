75182a88b31d6182877c9883bd974c528f9db0cf
Aldo <110924892+andreauxue@users.noreply.github.com>
2025-04-27 19:58:57 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { RegisterComponent } from './modules/usuario/register/register.component
 import { TemplateComponent } from './modules/usuario/template/template.component';
 import { ProfileComponent } from './modules/usuario/profile/profile.component';
 import { authenticationGuard } from './core/auth/authentication.guard';
+import { MapaIncidentesComponent } from './modules/incidente/mapa-incidentes/mapa-incidentes.component';
+import { ReportarIncidenteComponent } from './modules/incidente/reportar-incidente/reportar-incidente.component';
+
 export const routes: Routes = [
   {
     path: '',
@@ -30,7 +33,7 @@ export const routes: Routes = [
       },
     ]
   },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: 'map', pathMatch: 'full' }
 ];
 
 
